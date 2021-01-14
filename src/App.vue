3937242ecf5ac77277df40d820e64aea9c296d51
emilie-arquilliere <emilie.arquilliere@gmail.com>
2021-01-14 14:17:58 +0100

    projet api user en vueJS avec filtre des genres

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Fetch Users with VueJS</h1>
+  <br>
+  <button class="btn btn-primary" v-on:click="fetchUsers">FetchUsers</button>
+  <label> 
+    <input type="checkbox" v-model="genderFilter" value="male" />
+    Fetch male
+  </label>
+  <label>
+    <input type="checkbox" v-model="genderFilter" value="female" />
+    Fetch female
+  </label>
+  <TakeUser :usersFiltered="usersFiltered" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TakeUser from './components/TakeUser.vue'
+import axios from "axios"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TakeUser
+  },
+  data(){
+    return{
+      users: [],
+      genderFilter: ['male', 'female']
+    }
+  },
+  computed:{
+    usersFiltered(){
+      return this.users.filter((user) => (this.genderFilter.includes(user.gender)))
+    }
+  },
+  methods:{
+   async fetchUsers(){
+      await axios
+      .get("https://randomuser.me/api/?results=20")
+      .then(response => this.users = response.data.results);
+    }
   }
 }
 </script>
---
+++ src/components/TakeUser.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <table id="tbl-users" class="table table-hover">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Nom</th>
+          <th>Prénom</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in usersFiltered" :key="user">
+          <td><img :src="user.picture.thumbnail"/></td>
+          <td>{{user.name.last}}</td>
+          <td>{{user.name.first}}</td>
+          <td>{{user.email}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TakeUser',
+  props: {
+    usersFiltered: Array,
+    genderFilter : Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
