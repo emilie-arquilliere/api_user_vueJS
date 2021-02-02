9c306fd198aa5d5489f0fd94bd15636492782c62
emilie-arquilliere <emilie.arquilliere@gmail.com>
2021-02-02 15:31:07 +0100

    Modif suite à l'ajout de vue-router + ajout route vers users

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h1>Fetch Users with VueJS</h1>
-  <br>
-  <button class="btn btn-primary" v-on:click="fetchUsers">FetchUsers</button>
-  <label> 
-    <input type="checkbox" v-model="genderFilter" value="male" />
-    Fetch male
-  </label>
-  <label>
-    <input type="checkbox" v-model="genderFilter" value="female" />
-    Fetch female
-  </label>
-  <span>Nombre d'utilisateur : {{usersFiltered.length}}</span>
-  <br>
-  <label>
-    Search : 
-    <input type="search" v-model="search" />
-  </label>
-  <TakeUser :usersFiltered="usersFiltered" :sortDirection="sortDirection" :sort="sort" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/users">Users</router-link>
+  </div>
+  <router-view/>
 </template>
-
-<script>
-import TakeUser from './components/TakeUser.vue'
-import axios from "axios"
-
-export default {
-  name: 'App',
-  components: {
-    TakeUser
-  },
-  data(){
-    return{
-      users: [],
-      genderFilter: ['male', 'female'],
-      search: '',
-      sortDirection : 'asc',
-      sortKey : 'dob.age'
-    }
-  },
-  computed:{
-    usersFiltered(){
-      const filter = new RegExp(this.search, "i");
-      return this.users
-                  .filter( (user) => (this.genderFilter.includes(user.gender)) )
-                  .filter( user => user.name.last.match(filter) || user.name.first.match(filter) )
-                  .sort( (u1,u2) => {
-                    const modifier = this.sortDirection === 'desc' ? -1 : 1;
-                    return (u1.dob.age - u2.dob.age) * modifier;
-                  })
-    } 
-  }, 
-  methods:{
-    fetchUsers(){
-       axios
-      .get("https://randomuser.me/api/?results=20")
-      .then(response => this.users = this.users.concat(response.data.results))
-    },
-    sort(key){
-      if(this.sortKey === key) {
-          this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-      }
-    }
-  },
-  beforeMount(){
-    this.fetchUsers();
-  }
-}
-</script>
 
 <style>
 #app {
@@ -75,6 +14,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Users.vue
@@ -0,0 +1,78 @@
+<template>
+  <h1>Fetch Users with VueJS</h1>
+  <br>
+  <button class="btn btn-primary" v-on:click="fetchUsers">FetchUsers</button>
+  <label> 
+    <input type="checkbox" v-model="genderFilter" value="male" />
+    Fetch male
+  </label>
+  <label>
+    <input type="checkbox" v-model="genderFilter" value="female" />
+    Fetch female
+  </label>
+  <span>Nombre d'utilisateur : {{usersFiltered.length}}</span>
+  <br>
+  <label>
+    Search : 
+    <input type="search" v-model="search" />
+  </label>
+  <TakeUser :usersFiltered="usersFiltered" :sortDirection="sortDirection" :sort="sort" />
+</template>
+
+<script>
+import TakeUser from '../components/TakeUser.vue'
+import axios from "axios"
+export default {
+  name: 'App',
+  components: {
+    TakeUser
+  },
+  data(){
+    return{
+      users: [],
+      genderFilter: ['male', 'female'],
+      search: '',
+      sortDirection : 'asc',
+      sortKey : 'dob.age'
+    }
+  },
+  computed:{
+    usersFiltered(){
+      const filter = new RegExp(this.search, "i");
+      return this.users
+                  .filter( (user) => (this.genderFilter.includes(user.gender)) )
+                  .filter( user => user.name.last.match(filter) || user.name.first.match(filter) )
+                  .sort( (u1,u2) => {
+                    const modifier = this.sortDirection === 'desc' ? -1 : 1;
+                    return (u1.dob.age - u2.dob.age) * modifier;
+                  })
+    } 
+  }, 
+  methods:{
+    fetchUsers(){
+       axios
+      .get("https://randomuser.me/api/?results=20")
+      .then(response => this.users = this.users.concat(response.data.results))
+    },
+    sort(key){
+      if(this.sortKey === key) {
+          this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+      }
+    }
+  }/*,
+  beforeMount(){
+    this.fetchUsers();
+  }*/
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
